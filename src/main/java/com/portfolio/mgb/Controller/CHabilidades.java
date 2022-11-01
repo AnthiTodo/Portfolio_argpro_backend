c38e737eda3041d3a1afb2d64019f7019d217e8f
AntihTodo <curiosity.pesi@gmail.com>
2022-10-31 21:55:28 -0300

    un mensaje

--- src/main/java/com/portfolio/mgb/Controller/CHabilidades.java
+++ src/main/java/com/portfolio/mgb/Controller/CHabilidades.java
@@ -20,10 +20,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "https://mgbfrontend.web.app")
+@CrossOrigin(origins = "https://frontend-portfolio2022.web.app/")
 @RequestMapping("/skill")
-public class CHys {
+public class CHabilidades {
 
     @Autowired
     SHabilidades shys;
